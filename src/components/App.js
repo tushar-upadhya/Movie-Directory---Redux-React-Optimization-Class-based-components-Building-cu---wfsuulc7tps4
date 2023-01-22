9d2a52ecab778d8c0eff88c58834884fa759a8a1
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2023-01-22 19:18:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/App.css";
+import MovieProvider from "./MovieContext";
 import Movie from "./Movie";
 import MoviesList from "./MoviesList";
-export const tmp = React.createContext();
-export const tmp2 = React.createContext();
 
 const App = () => {
-  const [movie, setMovie] = useState({});
   return (
-    <div id="main">
-      <h1>Movie List</h1>
-      <tmp.Provider value={setMovie}>
+    <MovieProvider>
+      <div id="main">
+        <h1>Movie List</h1>
         <MoviesList />
-      </tmp.Provider>
-      <tmp2.Provider value={movie}>
         <Movie />
-      </tmp2.Provider>
-    </div>
+      </div>
+    </MovieProvider>
   );
 };
 
