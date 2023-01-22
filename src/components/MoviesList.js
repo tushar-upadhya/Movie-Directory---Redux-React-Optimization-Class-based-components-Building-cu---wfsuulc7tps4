bd0e228e46ec97527963a303585fb99a9ed477bf
Tushar Upadhyay <113006456+tushar-upadhya@users.noreply.github.com>
2023-01-22 19:20:15 +0530

    Update MoviesList.js

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,45 +1,38 @@
 import React from "react";
-import moviesList from "./MovieContext";
+import { MovieContext } from "./MovieContext";
 
-import { tmp } from "./App";
 class MoviesList extends React.Component {
+  static contextType = MovieContext;
+
   render() {
-    // Get movies list using MoviesContext.
+    const {movies, setState} =  this.context;
+
+    const handleOnClick = (id) => {
+      setState((state) => ({...state, selectedMovieId: id }));
+    }
 
     return (
-      <tmp.Consumer>
-        {(setMovie) => {
-          return (
-            <div id="movies-table">
-              <style>{`
+      <div id="movies-table">
+        <style>{
+        `
         td { padding : 20px; }
         tr:hover { background: #3374C2}
         `}</style>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Title</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr id="table-row">
-                    <td>Movie Title</td>
-                  </tr>
-                  {moviesList.map((item) => {
-                    return (
-                      <tr>
-                        <td key={item.id} onClick={() => setMovie(item)}>
-                          {item.title}
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
-          );
-        }}
-      </tmp.Consumer>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map((movie) => (
+              <tr id={`table-row-${movie.id}`} key={movie.id} onClick={() => handleOnClick(movie.id)}>
+                <td>{movie.title}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
